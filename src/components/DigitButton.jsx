513d2c7216ab7d493fbb72c5490f568c3e264717
DayTroy <shadowyguy5@gmail.com>
2023-07-25 01:56:37 +0300

    Almost implemented entering from keyboard

--- src/components/DigitButton.jsx
+++ src/components/DigitButton.jsx
@@ -1,7 +1,17 @@
-import React from "react";
+import React, { useEffect } from "react";
 import ACTIONS from "../utils/ACTIONS.JS";
 
 export default function DigitButton({ dispatch, digit }) {
+  const handleKeyDown = (event) => {
+    if (!isNaN(event.key) && event.key === digit) {
+      dispatch({ type: ACTIONS.ADD_DIGIT, payload: { digit: digit } });
+    }
+  };
+
+  useEffect(() => {
+    document.addEventListener("keydown", handleKeyDown);
+  }, []);
+  
   return (
     <button
       onClick={() => dispatch({ type: ACTIONS.ADD_DIGIT, payload: { digit } })}
--- src/components/OperationButton.jsx
+++ src/components/OperationButton.jsx
@@ -1,7 +1,18 @@
 import React from "react";
 import ACTIONS from "../utils/ACTIONS.JS";
+import { useEffect } from "react";
 
 export default function OperationButton({ dispatch, operation }) {
+  const handleKeyDown = (event) => {
+    if (event.key === operation) {
+      dispatch({ type: ACTIONS.CHOOSE_OPERATION, payload: { operation: operation } });
+    }
+  };
+
+  useEffect(() => {
+    document.addEventListener("keydown", handleKeyDown);
+  }, []);
+
   return (
     <button
       onClick={() =>
